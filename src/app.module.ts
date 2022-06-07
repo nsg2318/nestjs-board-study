6eeafa60454eca229714eb3d608c441a9c33ddc4
sunba <nsg2318@naver.com>
2022-06-07 10:25:15 +0900

    add : board repository 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './boards/configs/typeORMConfig';
+import { BoardRepository } from './boards/board.repository';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+  imports: [
+    TypeOrmModule.forFeature([BoardRepository]),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+  ],
 })
 export class AppModule {}
